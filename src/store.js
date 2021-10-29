b13036d3877036f278083dd3cb302aa263e080f1
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-10-29 15:50:12 +0200

    Make list draggable

--- src/store.js
+++ src/store.js
@@ -1,11 +1,46 @@
-
-const setToLocalStorage = ((toDo) => {
-  localStorage.setItem('toDo', JSON.stringify(toDo));
-});
-
-const getFromLocalStorage = () => {
-  const toDo = localStorage.getItem('toDo');
-  return JSON.parse(toDo);
+const toStorage = (data) => {
+  localStorage.setItem('todo', JSON.stringify(data));
 };
 
-export { setToLocalStorage, getFromLocalStorage };
+const fromStorage = () => {
+  const todoList = localStorage.getItem('todo');
+  return JSON.parse(todoList);
+};
+
+const markTodoItem = (index, value) => {
+  const list = fromStorage();
+
+  list.forEach((item) => {
+    if (item.index === Number(index) || item.index === index.toString()) {
+      item.completed = value;
+    }
+  });
+
+  toStorage(list);
+};
+
+const updateTodo = (todoItem) => {
+  const checkbox = todoItem.children[0].children[0];
+  const checkboxIndex = checkbox.getAttribute('name').split('-')[1];
+
+  if (checkbox.checked) {
+    markTodoItem(checkboxIndex, true);
+    checkbox.nextElementSibling.style.textDecoration = 'line-through';
+  } else {
+    markTodoItem(checkboxIndex, false);
+    checkbox.nextElementSibling.style.textDecoration = 'none';
+  }
+};
+
+const reloadStore = () => {
+  const todoItems = document.getElementsByClassName('todo-item');
+
+  [...todoItems].forEach((todoItem) => {
+    todoItem.children[0].children[0].addEventListener('change', () => {
+      updateTodo(todoItem);
+    });
+  });
+};
+exports.reloadStore = reloadStore;
+exports.toStorage = toStorage;
+exports.fromStorage = fromStorage;
