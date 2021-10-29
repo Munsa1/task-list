f478fbfb8e66b51638bdc5c698bf04c1702c6e2c
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-10-29 12:03:10 +0200

    Populate task list dynamically using javascript

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+const list = document.getElementById('items-list');
 
-function component() {
-  const element = document.createElement('div');
+// task array of objects
+const toDoList = [
+  {
+    description: 'Binge Netflix',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Ride a bike',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Start to-do list project',
+    completed: true,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+// populate list
+const populate = () => {
+  // sort the array
+  const sortedList = toDoList.sort((x, y) => x.index - y.index);
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  // iterate
+  for (let i = 0; i < sortedList.length; i += 1) {
+    list.insertAdjacentHTML('beforeend', `
+      <div class="task">
+        <div>
+          <input class="check" type="checkbox" name="item-${sortedList[i].index}">
+          <label for="item-${sortedList[i].index}">${sortedList[i].description}</label>
+        </div>
+        <div class="material-icons-outlined">
+          more_vert
+        </div>
+      </div>
 
-  element.appendChild(myIcon);
+      <hr>
+    `);
+  }
+};
 
-  return element;
-}
-
-document.body.appendChild(component());
+window.onload = populate();
